1b29dc57cbef456a92aed730d68f2d3dd07e0ea8
Shreek009 <srinivaskalaskar0@gmail.com>
2025-06-28 15:46:54 +0530

    Rock-Paper-Scissors

--- Mini-Projects/Rock-Paper-Scissors/index.js
+++ Mini-Projects/Rock-Paper-Scissors/index.js
@@ -1,22 +1,59 @@
-randomNumber = Math.random();
+function playGame(playerMove){
+    const computerMove = pickComputerMove();
+    let result;
 
+    if(playerMove === 'Rock'){
 
-function playerMove() {
-    const playerGame = playerMove();
-    const computerGame = computerMove();
-    if(playerGame === 'Rock' && computerGame){
+        if(computerMove === 'Rock') {
+            result ='Its a tie';
+        } else if(computerMove === 'Paper') {
+            result = 'You Win';
+        } else if(computerMove === 'Scissors') {
+            result = 'You Lose';
+        }
 
+    } else if(playerMove === 'Scissors') {
+
+        if(computerMove === 'Rock') {
+            result ='Its a Lose';
+        } else if(computerMove === 'Paper') {
+            result = 'You Win';
+        } else if(computerMove === 'Scissors') {
+            result = 'Its a Tie';
+        }
+
+    } else if(playerMove === 'Paper') {
+
+        if(computerMove === 'Rock') {
+            result ='You Lose';
+        } else if(computerMove === 'Paper') {
+            result = 'Its a Tie';
+        } else if(computerMove === 'Scissors') {
+            result = 'You Win';
+        }
+        
     }
+
+    alert(`You Picked ${playerMove}. Computer Picked ${computerMove}. ${result}`);
+
 }
 
-function computerMove() {
-    if(randomNumber >= 0 && randomNumber < (1/3) ) {
-        alert("Computer picked Rock");
-    }
-    else if(randomNumber > (1/3) && randomNumber < (2/3) ) {
-        alert("Computer picked paper");
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+    let computerMove = '';
+
+    if(randomNumber >=0 && randomNumber < 1/3) {
+        computerMove = 'Rock';
     } 
-    else{
-        alert("Computer picked Scissors");
+    else if(randomNumber > 1/3 && randomNumber < 2/3) {
+        computerMove = 'Paper';
+    } 
+    else if(randomNumber > 2/3 && randomNumber < 1) {
+        computerMove = 'Scissors';
     }
+    return computerMove;
 }
+
+
+
